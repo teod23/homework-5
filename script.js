75b583996bf7b8741b06d88344b3f253f2d2f650
teod23 <168969458+teod23@users.noreply.github.com>
2024-08-23 22:03:01 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let temperature = 35; 
+
+console.log(temperature === 0)
+
+if (temperature < 0) {
+ console.log('ყინავს!');
+} else if (temperature > 0 && temperature < 11) {
+    console.log ('ძალიან ცივა');
+} else if (temperature > 10 && temperature < 21) {
+    console.log('ცივა');
+} else if (temperature > 21 && temperature < 31) {
+    console.log('თბილა');
+}
+ else if(temperature > 30) {
+    console.log ('ცხელა!')
+}
